568cd7ad2bf297b93480a972f358afce7c6f9f3f
Bryan Dang <bryanvdang@gmail.com>
2022-01-18 22:31:05 -0700

    created dice simulator to practice states

---
+++ dice-simulator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RollDice from './RollDice.js';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
