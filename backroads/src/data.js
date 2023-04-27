642f50933acb2d97ac4e1a2d5459e9b3d7d0ec57
Bryan Dang <bryanvdang@gmail.com>
2023-04-26 20:07:45 -0700

    added more images and updated data for tour images

--- backroads/src/data.js
+++ backroads/src/data.js
@@ -1,5 +1,7 @@
-import tour1 from "./images/tour-1.jpeg";
-import tour2 from "./images/tour-2.jpeg";
+import tokyo from "./images/tokyo.jpeg";
+import osaka from "./images/Osaka.jpeg";
+import alaska from "./images/alaska.jpeg";
+import vietnam from "./images/Vietnam.jpeg";
 import tour3 from "./images/tour-3.jpeg";
 import tour4 from "./images/tour-4.jpeg";
 
@@ -55,26 +57,36 @@ export const services = [
 export const tours = [
   {
     id: 1,
-    image: tour1,
-    date: "august 26th, 2020",
-    title: "Tibet Adventure",
+    image: tokyo,
+    date: "September 26th, 2023",
+    title: "Tokyo Adventure",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
-    location: "china",
+    location: "Japan",
     duration: "6 days",
     price: "from $2100",
   },
   {
     id: 2,
-    image: tour2,
+    image: osaka,
     date: "october 1th, 2020",
-    title: "best of java",
+    title: "best of Osaka",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     location: "indonesia",
     duration: "11 days",
-    price: "from $1400",
+    price: "from $2000",
   },
   {
     id: 3,
+    image: alaska,
+    date: "december 5th, 2023",
+    title: "Northern Lights",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Alaska",
+    duration: "9 days",
+    price: "from $1300",
+  },
+  {
+    id: 4,
     image: tour3,
     date: "september 15th, 2020",
     title: "explore hong kong",
@@ -82,15 +94,5 @@ export const tours = [
     location: "hong kong",
     duration: "8 days",
     price: "from $5000",
-  },
-  {
-    id: 4,
-    image: tour4,
-    date: "december 5th, 2019",
-    title: "kenya highlights",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
-    location: "kenya",
-    duration: "20 days",
-    price: "from $3300",
   },
 ];
