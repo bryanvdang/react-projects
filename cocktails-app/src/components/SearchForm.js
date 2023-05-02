2db3b15b58cf4ed5c231705dbaffbf911771a48a
Bryan Dang <bryanvdang@gmail.com>
2023-05-02 09:08:43 -0700

    implement use ref to re-render cocktails displayed and update search form component

--- cocktails-app/src/components/SearchForm.js
+++ cocktails-app/src/components/SearchForm.js
@@ -3,10 +3,37 @@ import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchValue } = useGlobalContext();
+  const searchTerm = React.useRef("");
+
+  // set focus on input on page load
+  React.useEffect(() => {
+    searchTerm.current.focus();
+  }, []);
+
+  // call search on value change
+  const searchCocktail = () => {
+    setSearchValue(searchTerm.current.value);
+  };
+
+  // prevent user from using 'enter' key for search
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchTerm}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
