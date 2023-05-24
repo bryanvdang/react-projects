bb605cd7723a1f5933ec25dd476d8568a41f609b
Bryan Dang <bryanvdang@gmail.com>
2023-05-24 08:41:39 -0700

    updated header and footer component as well as added images folder and product.js that would resemble an api call

--- ecommerce/frontend/src/components/Header.js
+++ ecommerce/frontend/src/components/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function Header() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
   return (
-    <div>
-      <header>Header</header>
-    </div>
+    <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+      <Nav.Item>
+        <Nav.Link eventKey="1" href="#/home">
+          PROSHOP
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="2" href="/cart">
+          <i className="fas fa-shopping-cart"></i>Cart
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="3" href="/login">
+          <i className="fas fa-user"></i>Login
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 export default Header;
