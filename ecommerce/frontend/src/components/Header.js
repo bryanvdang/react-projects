322c33f1db7b8ddb97569bd0ae44c64252cfb984
Bryan Dang <bryanvdang@gmail.com>
2023-05-31 14:16:46 -0700

    update nav to use react router

--- ecommerce/frontend/src/components/Header.js
+++ ecommerce/frontend/src/components/Header.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import { Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
   return (
-    <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-          PROSHOP
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" href="/cart">
-          <i className="fas fa-shopping-cart"></i>Cart
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" href="/login">
-          <i className="fas fa-user"></i>Login
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Container>
+      <Nav variant="pills" activeKey="1">
+        <Nav.Item>
+          <NavLink eventKey="1" to="/">
+            PROSHOP
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink eventKey="2" to="/cart">
+            <i className="fas fa-shopping-cart"></i>Cart
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink eventKey="3" to="/login">
+            <i className="fas fa-user"></i>Login
+          </NavLink>
+        </Nav.Item>
+      </Nav>
+    </Container>
   );
 }
 export default Header;
