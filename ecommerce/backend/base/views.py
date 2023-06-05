d5590b86de734cb2b49e0a54331666c13f466be1
Bryan Dang <bryanvdang@gmail.com>
2023-06-04 22:02:42 -0700

    add django rest framework to diplay GET data from routes

--- ecommerce/backend/base/views.py
+++ ecommerce/backend/base/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .products import products
+
 # Create your views here.
 
 #function based views to show all the logic here vs class based views you can't and its a little bit more expert level
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    return Response('Hello')
 
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
 #configure URLs for views
