691bd1cd30e4879a354e8f9e77db3058a9ad23f6
Bryan Dang <bryanvdang@gmail.com>
2023-06-04 21:52:35 -0700

    updated products.py because key value wasn't in parantheses which was causing error

--- ecommerce/backend/base/views.py
+++ ecommerce/backend/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import Products
+from .products import products
 # Create your views here.
 
 #function based views to show all the logic here vs class based views you can't and its a little bit more expert level
