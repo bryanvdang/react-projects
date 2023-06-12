f97ad635283e276742ea22b80ef646f11151368a
Bryan Dang <bryanvdang@gmail.com>
2023-06-12 15:50:11 -0700

    create constants file to hold string constants. imported it to start using for switch statement

--- ecommerce/frontend/src/reducers/productReducers.js
+++ ecommerce/frontend/src/reducers/productReducers.js
@@ -1,12 +1,17 @@
 //reducer is what updates our store
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case "PRODUCT_LIST_REQUEST":
+    case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] };
-    case "PRODUCT_LIST_SUCCESS":
+    case PRODUCT_LIST_SUCCESS:
       // action.payload is what we're getting back from the API call
       return { loading: false, products: action.payload };
-    case "PRODUCT_LIST_FAIL":
+    case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
