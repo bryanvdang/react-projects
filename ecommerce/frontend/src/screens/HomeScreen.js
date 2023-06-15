ab06f8c4c0d293cbfb2947ec6e7b55375357615a
Bryan Dang <bryanvdang@gmail.com>
2023-06-14 22:47:21 -0700

    add loading message, error message with conditional

--- ecommerce/frontend/src/screens/HomeScreen.js
+++ ecommerce/frontend/src/screens/HomeScreen.js
@@ -8,22 +8,30 @@ function HomeScreen() {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { error, loading, products } = productList;
+  // destructure productList to provide products for page data mapping
 
   // useEffect get called everytime the component loads or when the state attribute gets updated
   useEffect(() => {
     dispatch(listProducts());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {/* if loading, render the h2 loading message, if error, load the h3 error message, else render the page with products */}
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
