5f0fc503731dea1e7c0fc1462dc228d8444b94f6
Bryan <bryanvdang@gmail.com>
2025-06-29 18:53:05 -0700

    update error component

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,14 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error Page</div>;
+  //create hook that will return the actual error
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">Back Home</Link>
+    </div>
+  );
 };
 export default Error;
--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const HomeLayout = () => {
   return (
     <div>
-      <nav>HomeLayout</nav>
+      <nav>nav bar</nav>
       <Outlet />
     </div>
   );
