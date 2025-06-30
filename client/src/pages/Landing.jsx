4f173c539ae98a6c5e6d09b4d2c9bb110dd00b86
Bryan <bryanvdang@gmail.com>
2025-06-29 21:23:46 -0700

    update landing page with css and images

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import mainImg from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify image" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login/Explore
+          </Link>
+        </div>
+        <img src={mainImg} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 export default Landing;
