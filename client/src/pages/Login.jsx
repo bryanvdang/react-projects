8b0a5ad1bd1fe2f19647f0933fe293a0625aa222
Bryan <bryanvdang@gmail.com>
2025-07-01 13:00:28 -0700

    implement login page component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="text" name="email" defaultValue="john@test.com" />
+        <FormRow type="password" name="password" defaultValue="test" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Not a member?
+          <Link to="/register" className="member-btn">
+            Register Here
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Login;
