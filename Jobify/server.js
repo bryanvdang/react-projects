afdf673473368f00d95cf179dbbac563ff18f268
Bryan <bryanvdang@gmail.com>
2025-07-11 20:43:00 -0700

    testing get and post in server.js. Added express and nodemon to package.json

--- Jobify/server.js
+++ Jobify/server.js
@@ -1,1 +1,16 @@
-console.log("Server is working....");
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+app.listen(5100, () => {
+  console.log("server is running...");
+});
